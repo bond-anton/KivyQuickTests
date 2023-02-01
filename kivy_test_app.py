581b8ae0ddd40bc6a09fea48629570ca9c6c77e0
bond_anton <bond.anton@gmail.com>
2023-02-01 20:32:22 +0300

    async app example

--- kivy_test_app.py
+++ kivy_test_app.py
@@ -1,14 +1,51 @@
-from kivy.app import App, async_runTouchApp
-from kivy.clock import Clock
+import datetime
+import numpy as np
+from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
-import time
+import trio
 
 Window.size = (400, 600)
 
 
-def long_calculation(m):
-    time.sleep(5)
+async def read_sensor_value():
+    result = np.random.random()
+    await trio.sleep(result)
+    return result
+
+
+async def task_consumer(processing_coro, receive_channel, name, *args, **kwargs):
+    print(f'Consumer {name} has started')
+    async with receive_channel:
+        async for value in receive_channel:
+            await processing_coro(value, *args, **kwargs)
+    print(f'Consumer {name} done')
+
+
+async def repetitive_task(coro, sender_channel, name, default_result=None, interval=1, repeats=-1,
+                          task_timeout=0.5, *args, **kwargs):
+    async with sender_channel:
+        try:
+            while repeats != 0:
+                print(f'Task {name} loop')
+                with trio.move_on_after(task_timeout) as time_out_scope:
+                    print(f'Task: {name} started')
+                    result = await coro(*args, **kwargs)
+                if time_out_scope.cancelled_caught:
+                    result = default_result
+                await sender_channel.send(result)
+                print('sensor value sent, sleep for', interval)
+                await trio.sleep(interval)
+                if repeats > 0:
+                    repeats -= 1
+        except trio.Cancelled:
+            print(f'Task {name} execution canceled')
+        finally:
+            print(f'Task {name} stopped')
+
+
+async def long_calculation(m):
+    await trio.sleep(5)
     result = {
         'x2': m * 2,
         'x3': m * 3,
@@ -17,35 +54,80 @@ def long_calculation(m):
     return result
 
 
+async def get_datetime():
+    await trio.sleep(0.1)
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
 class Container(GridLayout):
 
     def calculate(self):
+        self.calculate_button.disabled = True
+        app = App.get_running_app()
         try:
             m = float(self.input_number.text)
-        except ValueError as e:
+        except ValueError:
             m = 0
         print('Long Calculation Requested')
-
-        # result = long_calculation(m)
-        # self.x2.text = '{:.2f}'.format(result['x2'])
-        # self.x3.text = '{:.2f}'.format(result['x3'])
-        # self.x10.text = '{:.2f}'.format(result['x10'])
+        default = {'x2': 'x2', 'x3': 'x3', 'x10': 'x10'}
+        app.spawn_task(long_calculation, app.process_calculation_results,
+                       task_name='Long calculation', task_default_result=default, interval=0, repeats=1,
+                       task_timeout=10, task_args=(m,), task_kwargs=None,
+                       processor_name='Calculation processor', processor_args=None, processor_kwargs=None)
 
 
 class MyApp(App):
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.dt = 1.0
+    dt = 1.0
+    nursery = None
 
     def build(self):
-        Clock.schedule_interval(self.process_tasks, self.dt)
         return Container()
 
-    def process_tasks(self, *args):
-        print('wow', args)
+    async def app_func(self):
+
+        async with trio.open_nursery() as nursery:
+            self.nursery = nursery
+
+            async def run_wrapper():
+                await self.async_run(async_lib='trio')
+                print('App done')
+                nursery.cancel_scope.cancel()
+
+            nursery.start_soon(run_wrapper)
+
+    def spawn_task(self, task_coro, processor_coro, interval=1, repeats=-1,
+                   task_name=None, task_default_result=None, task_timeout=1, task_args=None, task_kwargs=None,
+                   processor_name=None, processor_args=None, processor_kwargs=None):
+        if task_name is None:
+            task_name = 'Task'
+        if processor_name is None:
+            processor_name = 'Processor'
+        if task_args is None:
+            task_args = ()
+        if processor_args is None:
+            processor_args = ()
+        if task_kwargs is None:
+            task_kwargs = {}
+        if processor_kwargs is None:
+            processor_kwargs = {}
+        sender, receiver = trio.open_memory_channel(0)
+        self.nursery.start_soon(task_consumer, processor_coro, receiver,
+                                processor_name, *processor_args, **processor_kwargs)
+        self.nursery.start_soon(repetitive_task, task_coro, sender,
+                                task_name, task_default_result, interval, repeats,
+                                task_timeout, *task_args, **task_kwargs)
+
+    async def process_calculation_results(self, value):
+        print(value)
+        self.root.ids.x2.text = '{:.2f}'.format(value['x2'])
+        self.root.ids.x3.text = '{:.2f}'.format(value['x3'])
+        self.root.ids.x10.text = '{:.2f}'.format(value['x10'])
+        self.root.ids.calculate_button.disabled = False
+
+    async def process_datetime(self, value):
+        self.root.ids.datetime.text = value
+
 
 if __name__ == '__main__':
-    # async_runTouchApp_func = partial(async_runTouchApp, async_lib='trio')
-    # trio.run(async_runTouchApp_func, MyApp())
-    MyApp().run()
+    trio.run(MyApp().app_func)
--- main.py
+++ main.py
@@ -1,16 +1,72 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import trio
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+async def main():
+    # send_channel, receive_channel = trio.open_memory_channel(0)
+    async with trio.open_nursery() as nursery:
+        # nursery.start_soon(producer, send_channel, 'P1')
+        # nursery.start_soon(consumer, receive_channel, 'C1')
+        # nursery.start_soon(producer, send_channel2, 'P2')
+        # nursery.start_soon(consumer, receive_channel2, 'C2')
+        for i in range(10):
+            print('I am here')
+            send_channel2, receive_channel2 = trio.open_memory_channel(0)
+            nursery.start_soon(consumer, receive_channel2, 'C2')
+            nursery.start_soon(single_task_action, long_operation, send_channel2, 'TASK1', None, 0.5, 10)
+            await trio.sleep(2)
+        nursery.cancel_scope.cancel()
 
 
-# Press the green button in the gutter to run the script.
+async def producer(send_channel, name):
+    async with send_channel:
+        try:
+            print(f'Producer {name} has started')
+            i = 0
+            while True:
+                with trio.move_on_after(5) as time_out_scope:
+                    await trio.sleep(np.random.random()*10)
+                    await send_channel.send(f'FROM: {name}, MSG: message {i}')
+                if time_out_scope.cancelled_caught:
+                    print('X' * 10)
+                    print(f'Producer {name} timeout: {time_out_scope.cancelled_caught}')
+                    print('X' * 10)
+                i += 1
+        except trio.Cancelled:
+            print(f'Producer {name} execution canceled')
+        finally:
+            print(f'Producer {name} done')
+    print(f'Producer {name} leaving the game')
+
+
+async def consumer(receive_channel, name):
+    print(f'Consumer {name} has started')
+    async with receive_channel:
+        async for value in receive_channel:
+            print(f'<== {name}: got value {value!r}')
+    print(f'Consumer {name} done')
+
+
+async def long_operation(m):
+    result = np.random.random()
+    await trio.sleep(result)
+    return result * m
+
+
+async def single_task_action(coro, sender_channel, name, default=None, timeout=0.5, *args, **kwargs):
+    async with sender_channel:
+        try:
+            with trio.move_on_after(timeout) as time_out_scope:
+                print(f'Task: {name} started')
+                result = await coro(*args, **kwargs)
+            if time_out_scope.cancelled_caught:
+                result = default
+            await sender_channel.send(f'FROM: {name}, MSG: {result}')
+        except trio.Cancelled:
+            print(f'Task {name} execution canceled')
+        finally:
+            print(f'Task {name} done')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    trio.run(main)
